df11c490d7fd23648ce77623b7a14ef990555809
Vishal Raj <vishalraj@Vishals-MacBook-Pro.local>
2023-08-24 12:21:01 +0530

    Created menuapi for multiple cards and added all the images

--- src/component/Basics/Restaurant.js
+++ src/component/Basics/Restaurant.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import "./style.css"
+import React from 'react';
+import "./style.css";
 
 const Restaurant = () => {
   return (
@@ -8,8 +8,15 @@ const Restaurant = () => {
         <div className = "card">
             <div className = "card-body">
                 <span className =  "card-number card-circle subtle">1</span>
-                <span className = "card-author subtle" style={{color: "red"}}>Breakfast</span>
+                <span className = "card-author subtle">Breakfast</span>
+                <h2 className = "card-title">Maggie</h2>
+                <span className = "card-description subtle">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi veniam nisi ad repellendus, debitis harum!
+                </span>
+                <div className = "card-read">Read</div>
             </div>
+            <img src="" alt= "images" className = "card-media" />
+            <span className = "card-tag subtle">Order Now</span> 
         </div>
     </div>
     </>
